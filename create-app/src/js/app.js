899cf2d71ac980863519c61d83a5879849ecde7e
Artur <golubev.artur.1986@gmail.com>
2022-11-16 16:17:42 +0300

    feat: add section our-blog, subscribe, and adaptive

--- create-app/src/js/app.js
+++ create-app/src/js/app.js
@@ -78,10 +78,10 @@ flsFunctions.initPopups();
 import * as flsForms from "./files/forms/forms.js";
 
 /* Работа с полями формы: добавление классов, работа с placeholder. */
-// flsForms.formFieldsInit();
+flsForms.formFieldsInit();
 
 /* Oтправка формы со встроенной валидацией полей. false - отключит валидацию */
-// flsForms.formSubmit(true);
+flsForms.formSubmit(true);
 
 /* (В работе) Работа с масками. */
 // flsForms.formMasks();
